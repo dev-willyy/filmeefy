50a4367c2e586b8bcbd920137026dd80f79a8c4c
Williams Madu <williams.madu97@gmail.com>
2023-08-18 17:16:47 +0100

    removed redundant files & directories from project repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import './App.css';
+import { Cloudinary } from '@cloudinary/url-gen';
+import { AdvancedVideo } from '@cloudinary/react';
 
 function App() {
+  // create & configure cloud instance
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: 'djwtajs1l',
+    },
+  });
+
+  // use video with public ID, 'video-tv-01_dphuok'
+  const homePageVideo = cld.video('video-tv-01_dphuok');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Filmeefy movie App</p>
-      </header>
+    <div className="h-screen flex justify-center items-center">
+      <AdvancedVideo cldVid={homePageVideo} autoPlay loop />
     </div>
   );
 }
